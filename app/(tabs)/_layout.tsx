d2d62c100eadec7ceed70ada85d1d952bdb65f3f
gabrielgbraga <ggoesbraga@gmail.com>
2025-08-08 15:11:48 -0300

    removed failed test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,6 @@ export default function Layout() {
       {/* Optionally configure static options outside the route.*/}
       <Stack.Screen name="index" options={{}} />
       <Stack.Screen name="managePat" options={{}} />
-      <Stack.Screen name="test" options={{}} />
       <Stack.Screen name="listing" options={{}} />
     </Stack>
   );
