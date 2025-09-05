a2635d4955ba1f282da1e7dba8370434759dfd67
gabrielgbraga <ggoesbraga@gmail.com>
2025-09-05 15:43:48 -0300

    fixed sark/light mode

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -11,8 +11,7 @@ export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  // const theme = useColorScheme() ?? 'dark';
-  const theme = 'dark';
+  const theme = useColorScheme() ?? 'dark';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
