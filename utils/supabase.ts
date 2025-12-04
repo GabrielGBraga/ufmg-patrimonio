f01d3347578728147c2b2c2f326818728f4d015c
gabrielgbraga <ggoesbraga@gmail.com>
2025-12-04 11:44:51 -0300

    Updated supabase link for zrok

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,7 +2,7 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://infectious-volvo-screening-weblogs.trycloudflare.com';
+const supabaseUrl = 'https://patrimonioufmg.share.zrok.io';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
